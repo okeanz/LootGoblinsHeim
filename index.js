9478ee80a375385f27b14de24deb886b5882c89c
okeanz <stargazer.lxfly@gmail.com>
2023-12-13 16:51:09 +0300

    -config

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-﻿const pjson = require('./package.json');
-// require modules
-const fs = require('fs');
+﻿const fs = require('fs');
 const archiver = require('archiver');
 
-// create a file to stream archive data to.
-const output = fs.createWriteStream(__dirname + `releases/LootGoblinsHeim-${pjson.version}.zip`);
-const archive = archiver('zip', {
-    zlib: { level: 5 } // Sets the compression level.
-});
+const pjson = require('./package.json');
+const CURRENT_PACKAGE_VERSION = pjson.version;
 
-// listen for all archive data to be written
-// 'close' event is fired only when a file descriptor is involved
-output.on('close', function() {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-});
+updateManifestVersion();
+archivePackage();
 
-archive.directory('package/', false);
 
-archive.finalize();
+
+function archivePackage() {
+    const output = fs.createWriteStream(__dirname + `/releases/LootGoblinsHeim-${CURRENT_PACKAGE_VERSION}.zip`);
+    const archive = archiver('zip', {
+        zlib: { level: 5 } // Sets the compression level.
+    });
+
+    output.on('end', function() {
+        console.log('Data has been drained');
+    });
+
+    output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+
+    archive.pipe(output);
+
+    archive.directory('package/', false);
+
+    archive.finalize().then(() => {
+        console.log(`/releases/LootGoblinsHeim-${CURRENT_PACKAGE_VERSION}.zip is ready to deploy`);
+    });
+}
+
+
+function updateManifestVersion() {
+    const fileName = './package/manifest.json';
+    const file = require(fileName);
+    
+    console.log(`Updating manifest.verion_number from [${file.version_number}] to [${CURRENT_PACKAGE_VERSION}]`);
+
+    file.version_number = CURRENT_PACKAGE_VERSION;
+
+    fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
+        if (err) return console.log(err);
+        console.log('writing to ' + fileName);
+    });
+}
